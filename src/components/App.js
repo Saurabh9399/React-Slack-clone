bdb7d38c653689ac941db8ab541497c1fb100621
Saurabh <saurabhj8087445@gmail.com>
2020-10-12 17:05:46 +0530

    Add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import {SignIn} from './';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { SignIn } from './';
 
-function App() {
-  return (
-    <div className="App">
-    <SignIn/>
-    </div>
-  );
+function Home(){
+  return(
+    <div>Home</div>
+  )
+}
+function Contact(){
+  return(
+    <div>Contact Us</div>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class SignIn extends Component {
   render() {
-    return <div>SignIn</div>;
+    return (
+      <div className="signin-form">
+          <h1>Sign In</h1>
+        <button>Sign In with Google / Sign Up with Google</button>
+      </div>
+    );
   }
 }
