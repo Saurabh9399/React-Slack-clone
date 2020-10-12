37eda880db6dde8131ebed381f182ae35ecee29d
Saurabh <saurabhj8087445@gmail.com>
2020-10-12 17:55:01 +0530

    adding user profile

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 export default class Sidebar extends Component {
     render() {
         return (
-            <div>
-                Sidebar
+          <div id="sidebar">
+            <div className="user-profile">
+                <div>
+                    <img src="" alt=""/>
+                </div>
+                <div>
+                    <h3>Ronald</h3>
+                </div>
             </div>
-        )
+            <div className="user-channels"></div>
+          </div>
+        );
     }
 }
